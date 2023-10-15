d2a7d5ec9d12ad9bf81999256c955e83b75f44b0
rigolinux <rigoberto7567@gmail.com>
2023-10-15 09:04:12 -0600

    clean project quasar

---
+++ src/issues/pages/IssuePage.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-h2">Issues: </h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
---
+++ src/issues/pages/ListPage.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-h2">List page</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
